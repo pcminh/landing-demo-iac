61b9afe618200527e0aa7d8444c5e2e02fbafc72
Phan Cong Minh <pcminh@users.noreply.github.com>
2025-10-20 03:15:43 +0700

    Init - Testing

---
+++ terraform/main.tf
@@ -0,0 +1,13 @@
+terraform {
+  backend "local" {}
+}
+
+provider "aws" {
+    region = "ap-southeast-1"
+}
+
+data "aws_caller_identity" "current" {}
+
+output "account_id" {
+  value = data.aws_caller_identity.current.account_id
+}
