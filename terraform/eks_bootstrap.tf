0c9853418af14911d4a816da2198720aa194d0a6
Phan Cong Minh <pcminh@users.noreply.github.com>
2025-10-23 00:17:09 +0700

    feature: bootstrap helm chart via terraform - part 1: no config

---
+++ terraform/eks_bootstrap.tf
@@ -0,0 +1,39 @@
+# This runs after EKS is deployed to bootstrap K8s-native deployment tools
+
+provider "helm" {
+  kubernetes = {
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+
+    exec = {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      command     = "aws"
+      # This requires the awscli to be installed locally where Terraform is executed
+      args = ["eks", "get-token", "--cluster-name", module.eks.cluster_name, "--region", var.aws_region]
+    }
+  }
+}
+
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    # This requires the awscli to be installed locally where Terraform is executed
+    args = ["eks", "get-token", "--cluster-name", module.eks.cluster_name, "--region", var.aws_region]
+  }
+}
+
+resource "helm_release" "argocd" {
+  name = "argocd"
+  repository = "https://argoproj.github.io/argo-helm"
+  chart = "argo-cd"
+  namespace = "argocd"
+  create_namespace = true
+
+  values = [
+    file("${path.module}/helm/argocd.values.yaml")
+  ]
+}
